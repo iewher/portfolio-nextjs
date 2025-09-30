5f9149b0e6da951166067cdc80ae23fca3282d34
iewher <goshagang@mail.ru>
2025-10-01 00:07:27 +0500

    fix: some edits on scss files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Banner from "@/components/banner";
 import styles from "./page.module.scss";
-import Quote from "@/components/quote";
 import Skills from "@/components/skills";
 import AboutMe from "@/components/aboutme";
 import Contacts from "@/components/contacts";
@@ -12,7 +11,6 @@ export default function Page() {
   return (
     <div className={styles.Page}>
       <Banner />
-      <Quote />
       <Skills />
       <AboutMe />
       <Contacts />
